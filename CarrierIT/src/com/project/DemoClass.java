1013579da4e2e918fe07f5d4de52339000e082c9
ravilella1234 <ravilella1234>
2019-11-26 16:32:26 +0530

    Added new class DemoClass

---
+++ CarrierIT/src/com/project/DemoClass.java
@@ -0,0 +1,10 @@
+package com.project;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is firstLine.....");
+	}
+
+}
