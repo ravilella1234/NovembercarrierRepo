0d3bb440652d10357696525437f143f06aa0ba95
ravilella1234 <37369566+ravilella1234@users.noreply.github.com>
2019-11-26 17:14:19 +0530

    Update DemoClass.java

--- CarrierIT/src/com/project/DemoClass.java
+++ CarrierIT/src/com/project/DemoClass.java
@@ -11,14 +11,6 @@ public class DemoClass {
 	}
 
 	
-	int a=10,b=20,c,result;
-	
-	public void add()
-	{
-		c=a+b;
-		System.out.println("Addition of A  & B is : " + c);
-	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is firstLine.....");
