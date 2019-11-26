b5671d955d2cb46ecdb2abced46ce342b26ae2ac
ravilella1234 <ravilella1234>
2019-11-26 17:06:32 +0530

    sfddsf

--- CarrierIT/src/com/project/DemoClass.java
+++ CarrierIT/src/com/project/DemoClass.java
@@ -2,6 +2,15 @@ package com.project;
 
 public class DemoClass {
 
+	
+	int a=10,b=20,c;
+	
+	public void add()
+	{
+		c=a+b;
+		System.out.println("Addition of A  & B is : " + c);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is firstLine.....");
