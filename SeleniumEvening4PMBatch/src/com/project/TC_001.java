6c958824010004c89b08ca1e8097a666dca46edc
ravilella1234 <masters.selenium@gmail.com>
2019-11-29 17:20:41 +0530

    wewew

---
+++ SeleniumEvening4PMBatch/src/com/project/TC_001.java
@@ -0,0 +1,17 @@
+package com.project;
+
+public class TC_001 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		launch("chromebrowser");
+		
+		navigateUrl("bestbuyurl");
+
+	}
+
+	
+}
